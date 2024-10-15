7869dd3bdbd66693fda31f8f19cba46f9f855177
Patrick Brochu <patrickbrochu70@gmail.com>
2024-10-15 15:53:04 -0400

    Working on change size behavior, tyring some this Anchor save here

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-console.log("hello world")
+/*Create the row container and the grid container*/ 
 let innerGridContainer = document.createElement("div")
 let gridContainer = document.querySelector(".gridContainer")
 gridContainer.append(innerGridContainer)
-
 innerGridContainer.style.display = "flex"
 
+/*Creation of the grid*/
 for( let x = 0; x<16;x++)
     {
         let rowContainer = document.createElement("div")
@@ -19,3 +19,29 @@ for( let x = 0; x<16;x++)
                 rowContainer.append(newSquareX)
             }
     }
+
+
+let promptAction = document.querySelector("button")
+promptAction.addEventListener("click", changeGridSize)
+
+function changeGridSize()
+{
+    let size = 0
+    gridContainer.innerHTML = ""
+    innerGridContainer.innerHTML = ""
+    size = prompt("Size of the grid?")
+    for( let x = 0; x<size;x++)
+        {
+            let rowContainerNew = document.createElement("div")
+            innerGridContainer.append(rowContainerNew)
+            for (let i = 0; i<size; i++)
+                {
+                    let newSquareXNew = document.createElement("div")
+                    newSquareXNew.style.border = "2px solid black"
+                    newSquareXNew.style.width= "64px"
+                    newSquareXNew.style.height= "64px"
+                    newSquareXNew.classList.add("hoveringColor")
+                    rowContainerNew.append(newSquareXNew)
+                }
+        }
+}
