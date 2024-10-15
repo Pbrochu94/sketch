72dece95e074e30aed93d8504fa3181295cb66f7
Patrick Brochu <patrickbrochu70@gmail.com>
2024-10-15 16:22:57 -0400

    EOD grid of changed size is added next to the old one instead of overwrite

--- script.js
+++ script.js
@@ -27,21 +27,19 @@ promptAction.addEventListener("click", changeGridSize)
 function changeGridSize()
 {
     let size = 0
-    gridContainer.innerHTML = ""
-    innerGridContainer.innerHTML = ""
     size = prompt("Size of the grid?")
     for( let x = 0; x<size;x++)
         {
-            let rowContainerNew = document.createElement("div")
-            innerGridContainer.append(rowContainerNew)
+            let rowContainer = document.createElement("div")
+            innerGridContainer.append(rowContainer)
             for (let i = 0; i<size; i++)
                 {
-                    let newSquareXNew = document.createElement("div")
-                    newSquareXNew.style.border = "2px solid black"
-                    newSquareXNew.style.width= "64px"
-                    newSquareXNew.style.height= "64px"
-                    newSquareXNew.classList.add("hoveringColor")
-                    rowContainerNew.append(newSquareXNew)
+                    let newSquareX = document.createElement("div")
+                    newSquareX.style.border = "2px solid black"
+                    newSquareX.style.width= "64px"
+                    newSquareX.style.height= "64px"
+                    newSquareX.classList.add("hoveringColor")
+                    rowContainer.append(newSquareX)
                 }
         }
 }
