8785634099b37850aca284163afd71a861ecc65e
Patrick Brochu <patrickbrochu70@gmail.com>
2024-10-17 10:45:42 -0400

    all the reset and new grid behavior is completes + reduce the border size to 1px instead of 2px - Still have to implement limit of 100 in prompt

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-/*Create the row container and the grid container*/ 
+/*Create the row container and the grid container + selectors and globa perimeters*/ 
 let innerGridContainer = document.createElement("div")
 let gridContainer = document.querySelector(".gridContainer")
 gridContainer.append(innerGridContainer)
 innerGridContainer.style.display = "flex"
-innerGridContainer.style.height = "1088px"
-innerGridContainer.style.width = "1088px"
+let size = 1088
+let promptAction = document.querySelector("button")
+promptAction.addEventListener("click", changeGridSize)
+
+/*create the size the grid must respect*/
+innerGridContainer.style.height = size+'px'
+innerGridContainer.style.width = size+'px'
 
 /*Creation of the grid*/
-for( let x = 0; x<16;x++)
+for( let x = 0; x<16;x++)/*create 16 vertical rows in the row container*/
     {
         let rowContainer = document.createElement("div")
         innerGridContainer.append(rowContainer)
-        for (let i = 0; i<16; i++)
+        for (let i = 0; i<16; i++)/*create 16 boxes in the row container*/
             {
                 let newSquareX = document.createElement("div")
-                newSquareX.style.border = "2px solid black"
+                newSquareX.style.border = "1px solid black"
                 newSquareX.style.width= "64px"
                 newSquareX.style.height= "64px"
                 newSquareX.classList.add("hoveringColor")
@@ -22,27 +27,27 @@ for( let x = 0; x<16;x++)
             }
     }
 
-
-let promptAction = document.querySelector("button")
-promptAction.addEventListener("click", changeGridSize)
-
 function changeGridSize()
 {
     innerGridContainer.innerHTML = ""
-
-    let size = 0
-    size = prompt("Size of the grid?")
-    for( let x = 0; x<size;x++)
+    let userNumber = prompt("Size of the grid?")
+    let height = 1088/userNumber /*Calculate the height per block to fill same grid space after change*/ 
+    for( let x = 0; x<userNumber;x++)/*create "user amount" of vertical rows in the row container*/
         {
             let rowContainer = document.createElement("div")
             innerGridContainer.append(rowContainer)
-            for (let i = 0; i<size; i++)
+
+            rowContainer.style.height = height+"px"/*Assign the size to each row to fill same grid space*/ 
+            rowContainer.style.width = "100%"
+            for (let i = 0; i<userNumber; i++)/*create "user amount" of horizontal boxes in the row container*/
                 {
                     let newSquareX = document.createElement("div")
-                    newSquareX.style.border = "2px solid black"
+                    newSquareX.style.border = "1px solid black"
                     newSquareX.style.width= "64px"
                     newSquareX.style.height= "64px"
                     newSquareX.classList.add("hoveringColor")
+                    newSquareX.style.height = height+"px"/*Assign the size to each box to fill same grid space*/ 
+                    newSquareX.style.width = "100%"
                     rowContainer.append(newSquareX)
                 }
         }
